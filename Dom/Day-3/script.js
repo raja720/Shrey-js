2ed7ffd2a2eafc28cb6aa95259bf3eecc649631b
Raja singh <162097745+raja720@users.noreply.github.com>
2025-03-26 15:16:16 +0530

    MAKING Aguess the team web

--- Dom/Day-3/script.js
+++ Dom/Day-3/script.js
@@ -9,23 +9,56 @@
 //     btn.style.color = `rgb(${c1},${c2},${c3})`
 // })
 
+// let btn = document.querySelector("button");
+// let h5 = document.querySelector("h5");
+
+// let check = 0;
+
+// btn.addEventListener("click",function(){
+//     if(check== 0){
+//         h5.innerHTML= "Friend";
+//         h5.style.color="green"
+//         btn.innerHTML = "Remove Friend"
+//         btn.style.backgroundColor = "green"
+//         check = 1;
+//     }else{
+//         h5.innerHTML= "Stranger";
+//         h5.style.color="red"
+//         btn.innerHTML = "Add Friend"
+//         btn.style.backgroundColor = "white"
+//         check = 0;
+//     }
+// })
+
+
+let arr = [ 
+    {team:'CSK',
+        primary:'yellow',
+        secondary:'pink'
+    },
+    {team:'RCB',
+        primary:'red',
+        secondary:'black'
+    },
+    {team:'MI',
+        primary:'green',
+        secondary:'orange'
+    }, 
+    {team:'TTW',
+        primary:'red',
+        secondary:'wheat'
+    },
+
+    ];
+ 
+
+
+let h1 = document.querySelector("h1");
 let btn = document.querySelector("button");
-let h5 = document.querySelector("h5");
-
-let check = 0;
-
 btn.addEventListener("click",function(){
-    if(check== 0){
-        h5.innerHTML= "Friend";
-        h5.style.color="green"
-        btn.innerHTML = "Remove Friend"
-        btn.style.backgroundColor = "green"
-        check = 1;
-    }else{
-        h5.innerHTML= "Stranger";
-        h5.style.color="red"
-        btn.innerHTML = "Add Friend"
-        btn.style.backgroundColor = "white"
-        check = 0;
-    }
+    let num = Math.floor(Math.random()*arr.length);
+    let ran = arr[num]
+    console.log(ran);
+    h1.innerHTML=ran.team;
+    h1.style.backgroundColor=ran.primary;
 })
