d4c93362298bde35564f4d82d96172c6b68e47a0
Raja singh <162097745+raja720@users.noreply.github.com>
2025-04-02 12:40:01 +0530

    changes

--- Dsa/Day-3(if-else)/script.js
+++ Dsa/Day-3(if-else)/script.js
@@ -35,23 +35,23 @@
 // }
 // console.log(amount);
 
-let unit = Number(prompt("enter your unit "));
+let unit = Number(prompt("enter your unit"));
 
 let amount = 0;
 
 if(unit>400){
-    amount=( unit-400)*13;
+    amount =(unit-400)*13;
     unit = 400;
-}
-if(unit>200 && unit<= 400){
-    amount = amount+(unit-200)*8;
+} if(unit>200 && unit<=400){
+    amount = amount + (unit-200)*8;
     unit = 200
-}
-if(unit>100 && unit<= 200){
-    amount= amount+(unit-100)*6;
-    unit=100
-}
 
-amount = amount+unit*4.2
+} if(unit>100 && unit<=200){
+    amount = amount + (unit - 100)*6;
+    unit = 100
+}
+    amount = amount + unit*4.2;
+
 
 console.log(amount);
+
