af545e0713c0add51e6f2712528d0f043d69532b
Raja singh <162097745+raja720@users.noreply.github.com>
2025-04-05 13:33:41 +0530

    nbj

--- Dsa/Assingments/script.js
+++ Dsa/Assingments/script.js
@@ -1,11 +1,22 @@
-let num = Number(prompt("Enter a number"));
-let copy = num;
-let sum = 0;
-while (num>0) {
-    let rem = num%10;
-    sum = sum+rem;
-    num = Math.floor(num/10);
-}
+// let num = Number(prompt("Enter a number"));
+// let copy = num;
+// let sum = 0;
+// while (num>0) {
+//     let rem = num%10;
+//     sum = sum+rem;
+//     num = Math.floor(num/10);
+// }
 
-if(copy%sum == 0) console.log("harsd");
-else console.log("not hasard");
+// if(copy%sum == 0) console.log("harsd");
+// else console.log("not hasard");
+
+let n = Number(prompt("enter a number"));
+let sum = 0;
+for (let i = 1; i <= n/2; i++) {
+    if(n%i == 0){
+       sum = sum +i; 
+    }
+   
+}
+if(sum>n)console.log("abundant number");
+else console.log("not anudant numebre");
